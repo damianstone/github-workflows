4b98a9dd6fc0646af86772706288d26586851d45
Damian Stone <damianstonedev@gmail.com>
2023-03-01 20:02:06 +0000

    addes aws bucket deployment

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,10 +1,20 @@
+// install => npm install @actions/core @actions/github @actions/exec
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // TODO: get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // TODO: upload files
+  const s3Uri = `s3://${bucket}`;
+  // exec.exec('aws s3 sync <local-folder> <s3-bucket>')
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice('Hello from my custom JS action');
 }
 run();
 
-// install => npm install @actions/core @actions/github @actions/exec
